56af4cc9e8ba290505c6223ece8d4c53dce80170
Luigio <ledragon0897@outlook.fr>
2025-08-21 12:05:21 +0200

    Clean project

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import logo from './logo.svg';
 // import './App.css';
 import Header from './components/header';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello World
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
     <div>
       <Header />
-      {/* Other components or content */}
-  </div>
+    </div>
   );
 }
 
