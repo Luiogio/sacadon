3182feb4d9f6b8de2fa2e6d31584fc16ac892ebd
Louis-Alexandre BOTTURA <ledragon0804@hotmail.fr>
2025-11-10 16:59:47 +0100

    Page Solution - Solution Section

--- src/components/solution/section_our_solution.jsx
+++ src/components/solution/section_our_solution.jsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 import './section_our_solution.css';
 
 import cycle from '../../assets/our_solution/cycle-de-vie.png';
@@ -9,32 +7,18 @@ export default function OurSolutionSection() {
     <div className="our-solution-section">
       <div className="titre">NOTRE SOLUTION</div>
 
-      <h3 className="soustitre">
+      <div className="soustitre">
         Notre solution : une réponse simple à des enjeux complexes
-      </h3>
-
-      {/* Texte explicatif */}
-      <div className="texte-section">
-        <p>
-          <strong>SACADON</strong> n’est pas une collection de sacs comme les
-          autres. C’est une solution concrète à deux urgences :{" "}
-          <strong>la pollution plastique</strong> &{" "}
-          <strong>les inégalités humaines</strong>.
-        </p>
-        <p>
-          Comment ? En créant un objet{" "}
-          <strong>réutilisable, recyclé, solidaire</strong>, et en intégrant{" "}
-          <strong>un don</strong> dans son prix de vente.
-        </p>
-        <p>
-          On appelle ça : <strong>un produit-partage.</strong>
-        </p>
       </div>
 
-      {/* Image du cycle de vie */}
-      <div className="image-container">
-        <img src={cycle} alt="Cycle de vie" className="image-cycle" />
-      </div>
+      <p className="texte-section">
+        SACADON n’est pas une collection de sacs comme les autres.<br/>
+        C’est une solution concrète à deux urgences : La <b>pollution plastique</b> & les <b>inégalités humaines</b>.<br/>
+        Comment ? En créant un objet <b>réutilisable, recyclé, solidaire</b>, et en intégrant <b>un don</b> dans son prix de vente.<br/>
+        On appelle ça : <b>un produit-partage.</b>
+      </p>
+
+      <img src={cycle} alt="Cycle de vie" className="image-cycle" />
     </div>
   );
 }
