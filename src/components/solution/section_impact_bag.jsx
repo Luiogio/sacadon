29a6bef914246097fdf9bb4d414fc5e92cf71a90
Louis-Alexandre BOTTURA <ledragon0804@hotmail.fr>
2025-11-11 12:46:14 +0100

    Page Solution - Humanity Section

--- src/components/solution/section_impact_bag.jsx
+++ src/components/solution/section_impact_bag.jsx
@@ -5,18 +5,18 @@ import './section_impact_bag.css';
 export default function ImpactBag() {
   return (
     <div className="impact-section">
-      <div className="impact-titre">Un sac à impact écologique</div>
+      <div className="impact-titre title">Un sac à impact écologique</div>
 
-      <p className="impact-soustitre">
+      <p className="impact-soustitre text-sac">
         Nos sacs sont fabriqués à partir de plastique PE recyclé, issu du tri
         sélectif des ménages.
       </p>
 
       <div className="impact-avantages">
-        <div className="carte">✔ Réduction des déchets</div>
-        <div className="carte">✔ Moins de pétrole utilisé</div>
-        <div className="carte">✔ Baisse des émissions de CO₂</div>
-        <div className="carte">✔ Soutien à une économie circulaire locale</div>
+        <div className="card-avantages">✔ Réduction des déchets</div>
+        <div className="card-avantages">✔ Moins de pétrole utilisé</div>
+        <div className="card-avantages">✔ Baisse des émissions de CO₂</div>
+        <div className="card-avantages">✔ Soutien à une économie circulaire locale</div>
       </div>
 
       <Link to="/solution" className="need-more-button impact-button">
--- src/components/solution/solution.jsx
+++ src/components/solution/solution.jsx
@@ -1,11 +1,13 @@
 import OurSolutionSection from './section_our_solution.jsx';
 import ImpactBag from './section_impact_bag.jsx';
+import Humanite from './section_humanite.jsx';
 
 export default function Solution() {
   return (
     <div className='body'>
       <OurSolutionSection />
       <ImpactBag />
+      <Humanite />
     </div>
   );
 }
