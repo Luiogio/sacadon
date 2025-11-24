ef442e0027c93ad3a05140bcb7adefc142b17618
Louis-Alexandre BOTTURA <ledragon0804@hotmail.fr>
2025-11-24 22:42:53 +0100

    Page Impact Ecological -  Response Section 1

--- src/components/impact_ecological/impact_ecological.jsx
+++ src/components/impact_ecological/impact_ecological.jsx
@@ -1,15 +1,13 @@
 import SectionEcological from './section_ecological.jsx';
 import SectionPlastic from './section_plastic.jsx';
-
-import FAQ from '../FAQ/section_faq.jsx';
+import SectionResponse from './section_response.jsx';
 
 export default function ImpactEcological() {
   return (
     <div className='body'>
       <SectionEcological />
       <SectionPlastic />
-
-      <FAQ />
+      <SectionResponse />
     </div>
   );
 }
