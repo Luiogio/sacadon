8d73e36c1cdf17f903e34028cdc5de2bbab28435
Louis-Alexandre BOTTURA <ledragon0804@hotmail.fr>
2025-12-08 20:53:30 +0100

    Page Mission CSS

--- src/components/home_page/section_history.jsx
+++ src/components/home_page/section_history.jsx
@@ -8,33 +8,29 @@ import logo_S from '../../assets/home_page/logo_S.png';
 export default function HistorySection() {
   return (
     <div className="history-section">
-      <div className="history-container">
+      <div className="history-image">
+        <img src={history_picture} alt="Notre histoire"  className="history-image"/>
+        <img src={logo_S} alt="Logo S"  className="history-logo"/>
+        
+        <Link to="/histoire" className="need-more-button">
+          En savoir plus +
+        </Link>
+      </div>
 
-        <div className="history-image">
-          <img src={history_picture} alt="Notre histoire"  className="image"/>
-          <img src={logo_S} alt="Logo S"  className="history-logo"/>
-          
-          <Link to="/histoire" className="need-more-button">
-            En savoir plus +
-          </Link>
+      <div className="history-content">
+        <div className="sacadon-title2 history-title">NOTRE HISTOIRE</div>
+
+        <div className="sacadon-text history-subtitle">
+          Une idée simple au service de tous : <br />
+          Former, informer, faire participer
         </div>
 
-        <div className="history-content">
-          <div className="sacadon-title2 history-title">NOTRE HISTOIRE</div>
-
-          <div className="sacadon-text history-subtitle">
-            Une idée simple au service de tous : <br />
-            Former, informer, faire participer
-          </div>
-
-          <div className="sacadon-text2 history-text">
-            Derrière SACADON, il y a l’histoire d’un <b>entrepreneur engagé</b>
-            (fondateur du <a href='https://www.handicap-international.fr/fr/actualites/sac-a-sapin' target="_blank">Sac à Sapin</a>),
-            d’une volonté <b>familiale et intergénérationnelle</b> de répondre à deux urgences : la
-            <b> pollution plastique</b> et la <b>solidarité</b>.
-            <br />
-            Le projet est porté par des jeunes créatifs et militants, pour redonner du sens à la consommation.
-          </div>
+        <div className="sacadon-text2 history-text">
+          Derrière SACADON, il y a une histoire,<br /> celle d’un <b>entrepreneur engagé</b>
+          (fondateur du <a href='https://www.handicap-international.fr/fr/actualites/sac-a-sapin' target="_blank">Sac à Sapin</a>),<br /> d’une volonté <br />
+          <b>familiale et intergénérationnelle</b> de répondre à deux <br />
+          urgences : la <b> pollution plastique</b> et la <b>solidarité</b>.<br />
+          Le projet est porté par des jeunes créatifs et militants, pour redonner du sens à la consommation.
         </div>
       </div>
     </div>
