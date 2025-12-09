bfee08b3d8ddcdafd8c88b6893d10be0226bd7b1
Louis-Alexandre BOTTURA <ledragon0804@hotmail.fr>
2025-12-10 00:11:15 +0100

    Header CSS

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,36 +11,38 @@ export default function Header({onShowSecond}) {
   const [open, setOpen] = useState(false);
 
   return (
-      <header>
-          <div className="header">
-              <div className="logo_header"><img src={logo} alt="Sacadon Logo"/></div>
-                  
-              <nav className="nav_header">
-                  <ul>
-                      <Link to="/" className="btn_header">Notre mission</Link>
-                      <Link to="/nos-sacs" className="btn_header">Nos sacs</Link>
+    <header>
+        <div className="header">
+            <div className="logo_header"><img src={logo} alt="Sacadon Logo"/></div>
+                
+            <nav className="nav_header">
+                <ul>
+                    <Link to="/" className="btn_header">Notre mission</Link>
+                    <Link to="/nos-sacs" className="btn_header">Nos sacs</Link>
+                    
+                    {/* MENU DEROULANT */}
+                    <div
+                    onMouseEnter={() => setOpen(true)}
+                    onMouseLeave={() => setOpen(false)}
+                    >
+                    <Link to="/impacts" className="btn_header impact">Impacts</Link>
+                    <nav className={`dropdown-menu ${open ? "open" : ""}`}>
+                        <ul>
+                            <Link to="/impact-ecologique" className="btn_nav_impact">Ecologique</Link>
+                            <Link to="/impact-humanitaire" className="btn_nav_impact">Humanitaire</Link>
+                            <Link to="/information-pedagogie" className="btn_nav_impact">Informations & pédagogie</Link>
+                        </ul>
+                    </nav>
+                    </div>
+                    {/* ---------------- */}
 
-                      <div
-                        onMouseEnter={() => setOpen(true)}
-                        onMouseLeave={() => setOpen(false)}
-                      >
-                        <Link to="/impacts" className="btn_header impact">Impacts</Link>
-                        <nav className={`dropdown-menu ${open ? "open" : ""}`}>
-                            <ul>
-                                <Link className="btn_nav_impact">Ecologique</Link>
-                                <Link className="btn_nav_impact">Humanitaire</Link>
-                                <Link className="btn_nav_impact">Informations & pédagogie</Link>
-                            </ul>
-                        </nav>
-                      </div>
-
-                      <Link to="/contact" className="btn_header">Contact</Link>
-                      <Link to="/nous-rejoindre" className="btn_header">Nous rejoindre</Link>
-                  </ul>
-              </nav>
-          </div>
-          
-          <img src={shop} alt="Sacadon Shop" className="shopping"/>
-      </header>
+                    <Link to="/contact" className="btn_header">Contact</Link>
+                    <Link to="/nous-rejoindre" className="btn_header">Nous rejoindre</Link>
+                </ul>
+            </nav>
+        </div>
+        
+        <img src={shop} alt="Sacadon Shop" className="shopping"/>
+    </header>
   );
 }
--- src/components/menu_responsive.jsx
+++ src/components/menu_responsive.jsx
@@ -13,32 +13,32 @@ export default function MenuResponsive({activeMenu}) {
   };
 
   return (
-        <div className={`menu_resp ${activeMenu ? "down" : ""}`}>
-            <nav>
-                <ul>
-                    <button className='menu_resp_'>Notre mission</button>
-                    <button className='menu_resp_'>Nos sacs</button>
-                    <button onClick={handleClick} className='impact_menu_resp'>
-                        <div>Impacts </div>
-                        <img 
-                            className={`img_rotated ${moved ? "down" : ""}`} 
-                            style={{transform: `rotate(${angle}deg)`}}
-                            src={bottom_arrow} alt="bottom arrow"
-                        />
-                    </button>
+    <div className={`menu_resp ${activeMenu ? "down" : ""}`}>
+        <nav>
+            <ul>
+                <button className='menu_resp_'>Notre mission</button>
+                <button className='menu_resp_'>Nos sacs</button>
+                <button onClick={handleClick} className='impact_menu_resp'>
+                    <div>Impacts </div>
+                    <img 
+                        className={`img_rotated ${moved ? "down" : ""}`} 
+                        style={{transform: `rotate(${angle}deg)`}}
+                        src={bottom_arrow} alt="bottom arrow"
+                    />
+                </button>
 
-                    <div className={`select_impact ${moved ? "down" : ""}`}>
-                        <button className='impact_menu'>Ecologique</button>
-                        <button className='impact_menu'>Humanitaire</button>
-                        <button className='impact_menu'>Informations & pédagogie</button>
-                    </div>
-                    
-                    <button className={`down_menu ${moved ? "down" : ""}`}>Contact</button>
-                    <button className={`down_menu ${moved ? "down" : ""}`}>Nous rejoindre</button>
-                    <button className={`shop_menu ${moved ? "down" : ""}`}>Panier</button>
-                </ul>
-            </nav>
-        </div>
+                <div className={`select_impact ${moved ? "down" : ""}`}>
+                    <button className='impact_menu'>Ecologique</button>
+                    <button className='impact_menu'>Humanitaire</button>
+                    <button className='impact_menu'>Informations & pédagogie</button>
+                </div>
+                
+                <button className={`down_menu ${moved ? "down" : ""}`}>Contact</button>
+                <button className={`down_menu ${moved ? "down" : ""}`}>Nous rejoindre</button>
+                <button className={`shop_menu ${moved ? "down" : ""}`}>Panier</button>
+            </ul>
+        </nav>
+    </div>
     );
 }
 
